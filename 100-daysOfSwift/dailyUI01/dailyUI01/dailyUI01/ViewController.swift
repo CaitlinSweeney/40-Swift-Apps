98f60b5f90db7a09b06679a49fb5e924996f370a
Caitlin sweeney <caitlin.marie.sweeney@gmail.com>
2017-02-07 14:49:06 -0700

    3 new apps

---
+++ 100-daysOfSwift/dailyUI01/dailyUI01/dailyUI01/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  dailyUI01
+//
+//  Created by caitlin sweeney on 2/7/17.
+//  Copyright © 2017 caitlin sweeney. All rights reserved.
+//
+
+import UIKit
+import PureLayout
+
+class ViewController: UIViewController {
+    
+    // MARK: View Did Load
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        assignbackground()
+
+    }
+    // MARK: Outlets
+    
+    
+    // MARK: Functions
+    func assignbackground(){
+        let background = UIImage(named: "BeachBG")
+        
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIViewContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubview(toBack: imageView)
+    }
+    
+
+}
+
